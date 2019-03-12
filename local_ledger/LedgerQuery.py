bdacb78b892eb904ba25cc6a333146cd596b7a27
Ryan West <ryanwest6@gmail.com>
2019-03-11 18:58:30 -0600

    PEP8 LedgerQuery.py
    Signed-off-by: Ryan West <ryanwest6@gmail.com>

--- local_ledger/LedgerQuery.py
+++ local_ledger/LedgerQuery.py
@@ -9,37 +9,39 @@ from Transaction import Transaction
 
 
 '''
-Provides more advanced queries to downloaded ledger data, including by timestamp
+Provides more advanced queries to downloaded ledger data, including by time
 and by range in time or sequence number.
 Requires a LedgerDownloader object.
 '''
 
-def getTxn(ledger, seqNo=None, timestamp=None):
-    '''Wrapper to get transaction by sequence number or timestamp''' 
-    
-    if ledger == None:
-        raise Exception('LocalLedger object must be provided') 
 
-    if seqNo == None and timestamp == None:
+def getTxn(ledger, seqNo=None, timestamp=None):
+    '''Wrapper to get transaction by sequence number or timestamp'''
+
+    if ledger is None:
+        raise Exception('LocalLedger object must be provided')
+
+    if seqNo is None and timestamp is None:
         raise Exception('Sequence number or timestamp required')
-    if seqNo != None and timestamp != None:
+    if seqNo is not None and timestamp is not None:
         raise Exception('Cannot provide both sequence number and timestamp')
 
     if isinstance(ledger, dict):
-        if seqNo != None:
+        if seqNo is not None:
             if seqNo in ledger:
                 return ledger[seqNo]
         else:
             _, txn = _getTxnByTimestamp(ledger, timestamp)
             return txn
     elif isinstance(ledger, LocalLedger):
-        if seqNo != None:
-           return ledger.getTxn(seqNo)
+        if seqNo is not None:
+            return ledger.getTxn(seqNo)
         else:
             _, txn = _getTxnByTimestamp(ledger, timestamp)
-            return txn 
+            return txn
     else:
         raise Exception('ledger parameter must be of type dict or LocalLedger')
+
 
 def getTxnCount(ledger):
     '''Gets number of transactions stored in the database'''
@@ -48,21 +50,22 @@ def getTxnCount(ledger):
         return ledger.getTxnCount()
     else:
         return len(ledger)
-    
+
+
 def _getTxnByTimestamp(ledger, timestamp, contiguous=True):
     '''
     finds closest transaction that occurred after given POSIX timestamp
     returns (txn, seqNo)
-    contiguous: if using a dict as input instead of LocalLedger and the dict does not
-        contain all transactions from one point in time to another, this must be set to false
-        or the method may fail. This slows down search time.
+    contiguous: if using a dict as input instead of LocalLedger and the dict
+        does not contain all transactions from one point in time to another,
+        this must be set to false or the method may fail. This slows down
+        search time.
     known bug: can't return txn seqNo <27 due to txn <17 with no timestamp
     '''
 
-    if timestamp == None:
+    if timestamp is None:
         return None
 
-    
     def binarySearch(l, r, ts):
         # Check base case
         if r >= l:
@@ -70,19 +73,19 @@ def _getTxnByTimestamp(ledger, timestamp, contiguous=True):
             curTxn = getTxn(ledger, mid)
             try:
                 txnTimestamp = curTxn.getTime()
-                if txnTimestamp == None:
+                if txnTimestamp is None:
                     raise Exception('txntimestamp returned None')
             except (KeyError, Exception) as e:
-                print('Txn ' +  str(mid) + ': txnTime in txnMetadata not found, ', end='')
-                # bug: sometimes 'txnMetadata' cannot be found, but later it can be.
-                # first ~17 transactions have no timestamps because they are genesis
-                # txns. If the binary search encounters one of these first transactions,
+                print('Txn ' + str(mid) +
+                      ': txnTime in txnMetadata not found, ', end='')
+                # first ~17 txns have no timestamps because they are genesis
+                # txns. If the binary search encounters one of these txns,
                 # it will fail and return the first transaction.
                 if mid < 17:
                     print('Reason: This is a genesis transaction')
                     print('Returning first genesis transaction')
                     return getTxn(ledger, 1)
-                else: 
+                else:
                     print('Reason: Unknown (Transaction JSON below)')
                     print(json.dumps(curTxn, indent=4))
                     raise e
@@ -97,11 +100,12 @@ def _getTxnByTimestamp(ledger, timestamp, contiguous=True):
             # Else the element can only be present
             # in right subarray
             else:
+
                 return binarySearch(mid + 1, r, ts)
         # if exact timestamp not found, return next in time
         else:
             return getTxn(ledger, l)
-   
+
     # if using LocalLedger, then all txns from the first are stored
     # contiguously
     if isinstance(ledger, LocalLedger):
@@ -118,26 +122,32 @@ def _getTxnByTimestamp(ledger, timestamp, contiguous=True):
     return seqNo, txn
 
 # get all transactions within a certain range
-def getTxnRange(ledger, startTime=None, endTime=None, startSeqNo=None, endSeqNo=None):
+
+
+def getTxnRange(ledger, startTime=None, endTime=None,
+                startSeqNo=None, endSeqNo=None):
     '''
-    Returns all transactions within a specified range of time. Can provide a 
-    starting and ending timestamp or an exact start and end sequence number. Both
-    start and end parameters are inclusive.
+    Returns all transactions within a specified range of time. Can provide a
+    starting and ending timestamp or an exact start and end sequence number.
+    Both start and end parameters are inclusive.
     '''
-    if startSeqNo == None:
-        startSeqNo, _ = _getTxnByTimestamp(ledger, startTime) 
-    if endSeqNo == None:
-        endSeqNo, txn = _getTxnByTimestamp(ledger, endTime) 
-        # since getTxnByTimestamp returns the NEXT transaction at or after the given
-        # timestamp, decrement ending sequence number if it is after the ending time
-        if 'txnTime' in txn['txnMetadata'] and txn['txnMetadata']['txnTime'] > endTime:
+    if startSeqNo is None:
+        startSeqNo, _ = _getTxnByTimestamp(ledger, startTime)
+    if endSeqNo is None:
+        endSeqNo, txn = _getTxnByTimestamp(ledger, endTime)
+        # since getTxnByTimestamp returns the NEXT transaction at or after the
+        # given timestamp, decrement ending sequence number if it is after the
+        # ending time
+        if 'txnTime' in txn['txnMetadata'] and \
+                txn['txnMetadata']['txnTime'] > endTime:
             endSeqNo -= 1
     if startSeqNo < 1 or endSeqNo < startSeqNo:
-        if (isinstance(ledger, LocalLedger) and endSeqNo > getTxnCount(ledger)) or \
-            (isinstance(ledger, dict) and endSeqNo > max(ledger.keys())):
+        if ((isinstance(ledger, LocalLedger) and
+            endSeqNo > getTxnCount(ledger)) or isinstance(ledger, dict and
+            endSeqNo > max(ledger.keys()))):
             raise Exception("invalid start/end times")
-    if startSeqNo == None or endSeqNo == None:
-        raise Exception('Must specify a start and end') 
+    if startSeqNo is None or endSeqNo is None:
+        raise Exception('Must specify a start and end')
 
     txns = {}
 
@@ -148,21 +158,17 @@ def getTxnRange(ledger, startTime=None, endTime=None, startSeqNo=None, endSeqNo=
         curSeqNo += 1
     return txns
 
+
 def getDidTxns(ledger, did):
     if isinstance(ledger, LocalLedger):
-        txns = getTxnRange(ledger, startSeqNo = 1, endSeqNo = ledger.getTxnCount()) 
+        txns = getTxnRange(ledger, startSeqNo=1, endSeqNo=ledger.getTxnCount())
         txns = txns.values()
     elif isinstance(ledger, dict):
-        txns = ledger.values() 
+        txns = ledger.values()
 
-    didTxns = {} 
+    didTxns = {}
     for t in txns:
-       if t.getSenderDid() == did:
-           didTxns[t.getSeqNo()] = t
-    
-    return didTxns
+        if t.getSenderDid() == did:
+            didTxns[t.getSeqNo()] = t
 
-   
-   
-   
-    
+    return didTxns
